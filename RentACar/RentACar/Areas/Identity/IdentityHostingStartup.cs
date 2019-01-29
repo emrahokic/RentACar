ed5df67670408e66daf19d303d89d163282583b9
emrah <emrah.okic@edu.fit.ba>
2019-01-29 22:43:06 +0100

    FirstCommit

---
+++ RentACar/RentACar/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RentACar.Data;
+
+[assembly: HostingStartup(typeof(RentACar.Areas.Identity.IdentityHostingStartup))]
+namespace RentACar.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
