b610e3f71f45763494e9ac5fafe3b9029a0dfa48
emrah <emrah.okic@edu.fit.ba>
2019-03-13 21:37:23 +0100

    PrikazSvihVozilaNaStranici

---
+++ RentACar/RentACar/Migrations/20190313162040_izmjenaAtributaUTrenutnaPoslovnica.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RentACar.Migrations
+{
+    public partial class izmjenaAtributaUTrenutnaPoslovnica : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DatumIzlaza",
+                table: "TrenutnaPoslovnica",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DatumIzlaza",
+                table: "TrenutnaPoslovnica",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+        }
+    }
+}
